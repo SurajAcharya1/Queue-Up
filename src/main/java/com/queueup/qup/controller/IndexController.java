fe784fbb3892d578ada87da27eb50e3f33ec5c80
suraj_ac1 <surajacharya13579@gmail.com>
2022-12-24 21:55:19 +0545

    changes

--- src/main/java/com/queueup/qup/controller/IndexController.java
+++ src/main/java/com/queueup/qup/controller/IndexController.java
@@ -45,8 +45,8 @@ public class IndexController {
 
     @GetMapping()
     public String openMainPage(Model model) {
-        tokenRepo.deleteTokenView();
         userRepo.createTokenViewAtFirst();
+        tokenRepo.deleteTokenView();
         model.addAttribute("userDto", new UserDto());
         Date date = new Date();
         SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy");
